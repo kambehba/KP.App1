8494fa33f4441739cc865dfe5a798145ff2b7e4b
kambehba <kamparsen@yahoo.com>
2015-12-06 11:46:08 -0500

    added view model to controller to populate view

--- app/project/project.controller.js
+++ app/project/project.controller.js
@@ -10,12 +10,15 @@
     function ProjectController($rootScope,firebaseDataService, projectService) {
         var vm = this;
         vm.gg = 'ssss2222ss';
+        vm.projects = [];
+        vm.project = { id: 0, status: '', title: '' };
       
 
         firebaseDataService.getProjects().then
                (
                        function (projects) {
-                           vm.projects = projects;
+                           vm.projects = projects.projects;
+                           vm.project = vm.projects[0];
                            var g = 5;
                        }
                );
